330ced354ded5710c9686ce9e14063bba6d09b3f
N0xCam <167630075+N0xCam@users.noreply.github.com>
2024-11-29 10:57:07 +0100

    footer

---
+++ src/app/products.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+  products: Product [] = [
+    {
+      id: 1,
+      name: 
+    }
+  ]
+  constructor() { }
+}
